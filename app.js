eac282a69315c8bcf286e50c83541f1a7bcb3ded
michalbara1 <michalbara1@gmail.com>
2023-06-10 15:54:53 +0300

    added navbar, input for search and start of logo

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ app.get('/', (req,res)=> {
 app.listen(port, function(){
 console.log('you connect to port 3300')
 
+app.get('/cart', (req,res)=> {
+    res.sendFile(path.join(__dirname, 'app.html'))
+   
+   })
 })
+//logo for the site
+const c = document.getElementById('myCanvas');
+const ctx = c.getContext('2d');
+var img = document.getElementById("logoImage");
+ctx.drawImage(img, 10, 10);
+ctx.font = "bold 40px Arial";
+ctx.textAllign='center';
+ctx.fillStyle= "#964B00";
+ctx.textBaeline='middle';
+const text='COCO Jewlery';
+ctx.fillText(text, c.width/2, c.height/2);
+
