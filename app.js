933c23e32fe5b252dc9ba287e80bc4b25987531c
michalbara1 <michalbara1@gmail.com>
2023-06-10 19:11:27 +0300

    added dropmenu to the navbar and a picture in the homepage

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ const ctx = c.getContext('2d');
 var img = document.getElementById("logoImage");
 ctx.drawImage(img, 10, 10);
 ctx.font = "bold 40px Arial";
-ctx.textAllign='center';
-ctx.fillStyle= "#964B00";
-ctx.textBaeline='middle';
-const text='COCO Jewlery';
+ctx.textAlign = 'center';
+ctx.fillStyle = "#964B00";
+ctx.textBaseline = 'middle';
+const text = 'COCO Jewelry';
 ctx.fillText(text, c.width/2, c.height/2);
+
 
