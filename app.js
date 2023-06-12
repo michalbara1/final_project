9cf23838d9402ceeaddae97ae6fc9aacd6772007
michalbara1 <michalbara1@gmail.com>
2023-06-12 19:07:01 +0300

    fixed the canvas, the server is linked to the right link in the nav bar and opend a new js

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-var express= require('express')
-app = express()
-const port= 3300
+// logo with canvas
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript code here
+    const c = document.getElementById('myCanvas');
+    const ctx = c.getContext('2d');
+    const img = document.getElementById("logoImage");
 
-const path= require('path')
-app.get('/', (req,res)=> {
- res.sendFile(path.join(__dirname, 'app.html'))
-
-})
-
-app.listen(port, function(){
-console.log('you connect to port 3300')
-
-app.get('/cart', (req,res)=> {
-    res.sendFile(path.join(__dirname, 'app.html'))
-   
-   })
-})
-//logo for the site
-const c = document.getElementById('myCanvas');
-const ctx = c.getContext('2d');
-var img = document.getElementById("logoImage");
-ctx.drawImage(img, 10, 10);
-ctx.font = "bold 40px Arial";
-ctx.textAlign = 'center';
-ctx.fillStyle = "#964B00";
-ctx.textBaseline = 'middle';
-const text = 'COCO Jewelry';
-ctx.fillText(text, c.width/2, c.height/2);
-
-
+    img.onload = function() {
+      ctx.drawImage(img, 10, 10);
+      ctx.font = "bold 40px Arial";
+      ctx.textAlign = 'center';
+      ctx.fillStyle = "#964B00";
+      ctx.textBaseline = 'middle';
+      const text = 'COCO Jewelry';
+      ctx.fillText(text, c.width/2, c.height/2);
+    };
+  });
---
+++ express.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var port = 3300;
+
+var path = require('path');
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app.html'));
+});
+
+
+app.get('/Contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Contact.html'));
+});
+
+app.get('/Rings', (req, res) => {
+  res.sendFile(path.join(__dirname, 'rings.html'));
+});
+
+app.get('/About', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+  });
+
+app.listen(port, function() {
+    console.log('you connect to port 3300');
+  });
+
+
+
+
+
+
+
